76c2d23c6f246f28ea4475fe55e7e8c9b301e18f
Theo LOPEZ <theolopez2000@gmail.com>
2022-02-24 12:05:17 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Voice Cloning - Projet M1 
+by Yohann & Théo 
+
+# Partie theorie 
+a faire 
+
+# Partie interface
+nous utilisons Flask 
+executer le fichier ./init.sh pour setup les environements 
+lancer votre localhost sur le port 5000
---
+++ __init__.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+from flask import Flask, render_template, request
+from subprocess import run, PIPE
+import subprocess
+
+UPLOAD_FOLDER = 'static/music/'
+ALLOWED_EXTENSIONS = {'mp3','wav'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['MAX_CONTENT_PATH'] = 32767              
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def index():
+    
+    if request.method == 'POST':
+        if request.form.get('Process'):
+            subprocess.call("./demo_cli.py", shell=True)
+
+        elif request.form.get('to_say'):
+            #on recupere ce qui a été mis dans le formulaire et on enregistre le tout dans un fichier txt
+            result = request.form['to_say']
+            file = open("static/to_say.txt", "w")
+            file.write(result)
+            file.close() 
+
+    return render_template("index.html")
+
+@app.route('/audio', methods=['POST'])
+def audio():
+    with open('static/music/audio_from_interface.wav', 'wb') as f:
+        f.write(request.data)
+    proc = run(['ffprobe', '-of', 'default=noprint_wrappers=1', 'static/music/audio_from_interface.wav'], text=True, stderr=PIPE)
+    return proc.stderr
+
+@app.route("/about")
+def about_page():
+    return render_template("about_us.html")
+    
+@app.route("/readme")
+def read_me():
+    return render_template("read_me.html")
+
+
+@app.route("/loading")
+def loading():
+    return render_template("loading.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
